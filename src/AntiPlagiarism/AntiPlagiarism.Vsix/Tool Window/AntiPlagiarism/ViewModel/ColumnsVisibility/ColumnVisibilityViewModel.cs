ec6c0301536aef0af2a0d909ebf7ffc8dca9fa98
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-04 02:16:04 +0300

    CLO-4: work on column visibility view model

--- src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnVisibilityViewModel.cs
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnVisibilityViewModel.cs
@@ -9,8 +9,29 @@ namespace AntiPlagiarism.Vsix.ToolWindows
 {
     public class ColumnVisibilityViewModel : ViewModelBase
 	{
-		
+		private bool _isVisible;
 
-		
+		public bool IsVisible
+		{
+			get => _isVisible;
+			set
+			{
+				if (_isVisible != value)
+				{
+					_isVisible = value;
+					NotifyPropertyChanged();
+				}
+			}
+		}
+
+		public string ColumnName { get; }
+
+		public ColumnVisibilityViewModel(string columnName, bool isVisible = true)
+		{
+			columnName.ThrowOnNullOrWhiteSpace(nameof(columnName));
+
+			ColumnName = columnName;
+			_isVisible = isVisible;
+		}
 	}
 }
