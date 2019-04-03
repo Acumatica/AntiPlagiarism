ba421d61cd7d5bf0a657f56144e164d250699ebe
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-04 02:03:41 +0300

    CLO-4: Added view models for column visibility

---
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnVisibilityViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AntiPlagiarism.Core.Method;
+using AntiPlagiarism.Core.Plagiarism;
+using AntiPlagiarism.Core.Utilities;
+using AntiPlagiarism.Vsix.Utilities;
+using System.Threading.Tasks;
+
+
+namespace AntiPlagiarism.Vsix.ToolWindows
+{
+    public class ColumnVisibilityViewModel : ViewModelBase
+	{
+		
+
+		
+	}
+}
---
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnsVisibilityCollectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using AntiPlagiarism.Core.Method;
+using AntiPlagiarism.Core.Plagiarism;
+using AntiPlagiarism.Core.Utilities;
+using AntiPlagiarism.Vsix.Utilities;
+using System.Threading.Tasks;
+
+
+namespace AntiPlagiarism.Vsix.ToolWindows
+{
+    public class ColumnsVisibilityCollectionViewModel : ViewModelBase
+	{
+		public AntiPlagiarismWindowViewModel ParentViewModel { get; }
+
+		public ColumnsVisibilityCollectionViewModel(AntiPlagiarismWindowViewModel parentViewModel)
+		{
+			parentViewModel.ThrowOnNull(nameof(parentViewModel));
+
+			ParentViewModel = parentViewModel;
+		}
+	}
+}
