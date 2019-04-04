3b0b83b4e00f329cefa62d94feb8a70722dbc1e5
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-04 03:01:32 +0300

    CLO-4: added collection view model

--- src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnsVisibilityCollectionViewModel.cs
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/ColumnsVisibility/ColumnsVisibilityCollectionViewModel.cs
@@ -1,21 +1,33 @@
-﻿using AntiPlagiarism.Core.Method;
-using AntiPlagiarism.Core.Plagiarism;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using AntiPlagiarism.Core.Utilities;
 using AntiPlagiarism.Vsix.Utilities;
-using System.Threading.Tasks;
+
 
 
 namespace AntiPlagiarism.Vsix.ToolWindows
 {
     public class ColumnsVisibilityCollectionViewModel : ViewModelBase
 	{
+		private readonly Dictionary<string, ColumnVisibilityViewModel> _columnVisibilitiesByName;
+		private readonly ObservableCollection<ColumnVisibilityViewModel> _columns;
+
 		public AntiPlagiarismWindowViewModel ParentViewModel { get; }
 
-		public ColumnsVisibilityCollectionViewModel(AntiPlagiarismWindowViewModel parentViewModel)
+		public ReadOnlyObservableCollection<ColumnVisibilityViewModel> Columns { get; }
+
+		public ColumnsVisibilityCollectionViewModel(AntiPlagiarismWindowViewModel parentViewModel,
+													IEnumerable<ColumnVisibilityViewModel> columnVMs)
 		{
 			parentViewModel.ThrowOnNull(nameof(parentViewModel));
+			columnVMs.ThrowOnNull(nameof(columnVMs));
 
 			ParentViewModel = parentViewModel;
+			_columnVisibilitiesByName = columnVMs.ToDictionary(colName => colName.ColumnName);
+			_columns = new ObservableCollection<ColumnVisibilityViewModel>(_columnVisibilitiesByName.Values);
+			Columns = new ReadOnlyObservableCollection<ColumnVisibilityViewModel>(_columns);
 		}
 	}
 }
