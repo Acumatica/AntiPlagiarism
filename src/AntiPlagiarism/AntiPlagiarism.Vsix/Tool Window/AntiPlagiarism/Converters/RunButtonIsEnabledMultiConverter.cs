93afbedb29ec142c350d25b0dc7f19fd1fa2327e
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-15 13:52:05 +0300

    CLO-3: Added support for Run button IsEnabled property for self analysis work mode via multiconverter

---
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/Converters/RunButtonIsEnabledMultiConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using AntiPlagiarism.Core.Utilities;
+
+
+namespace AntiPlagiarism.Vsix.ToolWindows.Converters
+{
+	/// <summary>
+	/// Converter which converts <see cref="AntiPlagiarismWindowViewModel.SelectedWorkMode.WorkMode"/>, <see cref="AntiPlagiarismWindowViewModel.ReferenceSolutionPath"/>
+	/// and <see cref="AntiPlagiarismWindowViewModel.IsAnalysisRunning"/> into <see cref="bool"/>  value for the Run button IsEnabled property.
+	/// </summary>
+	public class RunButtonIsEnabledMultiConverter : IMultiValueConverter
+	{
+	
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (values == null || values.Length != 3 || !(values[0] is WorkMode workMode) ||
+				!(values[1] is string referenceSolutionPath) || !(values[2] is bool analysisIsRunning))
+			{
+				return Binding.DoNothing;
+			}
+
+			bool isEnabled = !analysisIsRunning && (workMode == WorkMode.SelfAnalysis || !referenceSolutionPath.IsNullOrWhiteSpace());
+			return isEnabled;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
