696080739d44bc1503eac1cab81e137da479ef95
Nikomarov Sergey <snikomarov@acumatica.com>
2019-04-15 18:05:17 +0300

    CLO-3: fixed validation messages

--- src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/AntiPlagiarismWindowViewModel.cs
+++ src/AntiPlagiarism/AntiPlagiarism.Vsix/Tool Window/AntiPlagiarism/ViewModel/AntiPlagiarismWindowViewModel.cs
@@ -116,7 +116,8 @@ namespace AntiPlagiarism.Vsix.ToolWindows
 			
 				if (value < 0 || value > 100)
 				{
-					throw new ArgumentOutOfRangeException("The value should be in range between 0 and 100");
+					throw new ArgumentOutOfRangeException(message: "The value should be in range between 0 and 100",
+														  innerException: null);
 				}
 
 				_threshholdPercent = value;
@@ -144,8 +145,8 @@ namespace AntiPlagiarism.Vsix.ToolWindows
 
 				if (value < MinCheckedMethodSizeLowerBound || value > MinCheckedMethodSizeUpperBound)
 				{
-					throw new ArgumentOutOfRangeException(
-						$"The value should be in range between {MinCheckedMethodSizeLowerBound} and {MinCheckedMethodSizeUpperBound}");
+					throw new ArgumentOutOfRangeException(message: $"The value should be in range between {MinCheckedMethodSizeLowerBound} and {MinCheckedMethodSizeUpperBound}",
+														  innerException: null);
 				}
 
 				_minCheckedMethodSize = value;
